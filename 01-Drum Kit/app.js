b8e3d1690f8fbc1d01da31ef31e0d011705f8077
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-02-28 12:16:11 +0300

    init project

---
+++ 01-Drum Kit/app.js
@@ -0,0 +1,23 @@
+const btnA = document.getElementById('a');
+const btnS = document.getElementById('s');
+const btnD = document.getElementById('d');
+const btnF = document.getElementById('f');
+const btnG = document.getElementById('g');
+const btnH = document.getElementById('h');
+const btnJ = document.getElementById('j');
+const btnK = document.getElementById('k');
+const btnL = document.getElementById('l');
+
+function fire(e) {
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!key) {
+    return;
+  }
+  key.classList.toggle('playing');
+
+  setTimeout(() => {
+    key.classList.toggle('playing');
+  }, 100);
+}
+
+window.addEventListener('keydown', fire);
